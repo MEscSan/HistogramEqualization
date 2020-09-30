8f07798bd3934049709f2a1472f4c771499f57d6
MEscSan <ct4612@gmail.com>
2020-09-30 17:41:14 +0200

    Now

--- cuda/HistogramBenchmarking.cu
+++ cuda/HistogramBenchmarking.cu
@@ -246,6 +246,18 @@ int main(int argc, char* argv[])
     cout<< "\nShared Memory per Block:\t" << deviceProperties.sharedMemPerBlock/1024 << " kB";
     cout<< "\nShared Memory per SM:\t\t" << deviceProperties.sharedMemPerMultiprocessor/1024 << " kB";
     cout<< "\nTotal Global Memory:\t\t" << deviceProperties.totalGlobalMem/1024/1024/1024 << " GB";
+    
+    cout<<"\nColor Conversion using Unified memory\n";
+    
+    cout<<"Blocks \tThreads\t";
+    cout<<"GPU[ms]\tCPU[ms]\tSpeedUp(Cuda-Device with respect to CPU)\n";     
+    for(int threadsPerBlock = 32; threadsPerBlock<512; threadsPerBlock*=2)
+    {
+        int blocks = numSM*maxThreadsPerSM/threadsPerBlock;
+        cout<< blocks<<'\t' << threadsPerBlock;
+        ColorConversionUnifiedBenchmarking(blocks, threadsPerBlock);
+    }
+    
     cout<<"\n\nColor Conversion using pageable memory\n";
     
     cout<<"Blocks \tThreads\t";
